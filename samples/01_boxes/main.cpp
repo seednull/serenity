e3e30c46ea860ab3859f47a133d733369a08f318
Andrey Viktorov <andrey.viktorov@gmail.com>
2026-09-09 23:28:05 +0700

    Fixed sample

--- samples/01_boxes/main.cpp
+++ samples/01_boxes/main.cpp
@@ -1,20 +1,28 @@
 #include <serenity.h>
-#include <vector>
 #include <cassert>
 
 int main()
 {
+	Serenity_CapacityDesc capacity_desc = {};
+	capacity_desc.max_elements = 256;
+	capacity_desc.max_frame_arena_size = 64 * 1024;
+
+	uint64_t memory_size = serenityGetRequiredMemory(&capacity_desc);
+	uint8_t *memory = new uint8_t[memory_size];
+
+	Serenity_InstanceDesc desc = {};
+	desc.capacity = capacity_desc;
+	desc.memory_size = memory_size;
+	desc.memory = memory;
+
 	Serenity_Instance instance = SERENITY_NULL_HANDLE;
 
-	Serenity_InstanceDesc instance_desc =
-	{
-	};
-
-	Serenity_Result result = serenityCreateInstance(&instance_desc, &instance);
+	Serenity_Result result = serenityCreateInstance(&desc, &instance);
 	assert(result == SERENITY_SUCCESS);
 
 	result = serenityDestroyInstance(instance);
 	assert(result == SERENITY_SUCCESS);
 
+	delete[] memory;
 	return 0;
 }
